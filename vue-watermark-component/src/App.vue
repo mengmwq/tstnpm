793159ee9f7a99d4a13ba5fc6368d0a5179e231b
孟文琴 <mengwenqin@163.com>
2024-08-30 13:08:45 +0800

    first commit

---
+++ vue-watermark-component/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <watermark text="mjkmjk">
+      <h1>This is a test page</h1>
+    </watermark>
+  </div>
+</template>
+
+<script>
+import Watermark from "./components/Watermark.vue";
+
+export default {
+  name: "App",
+  components: {
+    Watermark,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-watermark-component/src/components/Watermark.vue
@@ -0,0 +1,37 @@
+<template>
+  <div style="position: relative">
+    <slot></slot>
+    <div :style="watermarkStyle">{{ text }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Watermark",
+  props: {
+    text: {
+      type: String,
+      default: "Default Watermark",
+    },
+    textStyle: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  computed: {
+    watermarkStyle() {
+      return {
+        position: "absolute",
+        zIndex: 10,
+        color: "rgba(0, 0, 0, 0.15)",
+        fontSize: "20px",
+        userSelect: "none",
+        pointerEvents: "none",
+        ...this.textStyle,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped></style>
